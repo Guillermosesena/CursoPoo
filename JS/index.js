6236295c728f4795039ebc6f10b7267ded75415f
Guillermosesena <80175905+Guillermosesena@users.noreply.github.com>
2021-06-24 22:33:20 -0500

    23/37 Se crean méetodos constructores en Java y JavaScript

---
+++ JS/index.js
@@ -0,0 +1,3 @@
+var car = new Car("AWED56", new Account("Andres Herrera","dwqa6"));
+car.passenger =4; 
+car.printDataCar();
--- Java/Main.java
+++ Java/Main.java
@@ -1,15 +1,16 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andre Herrera";
+
+        Car car = new Car("AMQ123", new Account("Andre Herrera", "AND123"));
+        //car.license = "AMQ123";
+        //car.driver = "Andre Herrera";
         car.passengers = 4;
         car.printDataCar();
 
-        Car car2 = new Car();
-        car2.license = "RGREGR";
-        car2.driver = "Andre Herrera";
+        Car car2 = new Car("RGREGR", new Account("Andre Herrera", "AND124"));
+        //car2.license = "RGREGR";
+        //car2.driver = "Andre Herrera";
         car2.passengers = 2;
         car2.printDataCar();    
     }
