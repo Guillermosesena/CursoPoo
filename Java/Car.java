6aa647d3e323a46e11a7b3fcf9a76fe64cf073f4
Guillermosesena <80175905+Guillermosesena@users.noreply.github.com>
2021-06-25 22:50:44 -0500

    33/37 Encapsulation in Java

--- Java/Car.java
+++ Java/Car.java
@@ -2,13 +2,27 @@ public class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passengers;
+    private Integer passengers;
     
     public Car(String license, Account driver){
         this.license = license;
         this.driver = driver;
     }
     void printDataCar(){
-        System.out.println("License: " + license + " Driver: " + driver.name);
+        System.out.println("License: " + license + " Driver: " + driver.name
+        + " Passegers: " + passengers);
+    }
+
+    public Integer getPassegers()
+    {   
+        return this.passengers;
+    }
+
+    public void setPassegers(Integer passengers)
+    {
+        if(passengers == 4)
+            this.passengers=passengers;
+        else
+            System.out.println("You must assign four passengers");
     }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,20 +1,12 @@
 class Main{
     public static void main(String[] args) {
-        //System.out.println("Hola mundo");
-
-        /*Car car = new Car("AMQ123", new Account("Andre Herrera", "AND123"));
-        car.passengers = 4;
-        car.printDataCar();
-
-        Car car2 = new Car("RGREGR", new Account("Andre Herrera", "AND124"));
-        car2.passengers = 2;
-        car2.printDataCar();        
-        
+ 
         UberX uberX= new UberX("RGREGR", new Account("Andre Herrera", "AND124"),"Chevrolet","Spark");
-        uberX.printDataCar();*/
-
-        User user = new User("Juan Vazquez","DQW9821");
+        uberX.setPassegers(4);
+        uberX.printDataCar();
+        
+        /*User user = new User("Juan Vazquez","DQW9821");
         user.auth("juanvazque@gmail.com", "123");
-        user.printDataAccount();
+        user.printDataAccount();*/
     }
 }
